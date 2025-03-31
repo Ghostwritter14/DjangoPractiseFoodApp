96e7e9198ca8e7634c2656a14cc6936f3fe3a7dd
soham <sohamrvd@gmail.com>
2025-03-31 18:16:47 +0100

    registered the data model and added it in my container

--- food/models.py
+++ food/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    def __str__(self):
+        return self.item_name
+    item_name = models.CharField(max_length=50)
+    item_description = models.CharField(max_length=200)
+    item_price = models.DecimalField(max_digits=5, decimal_places=2)
