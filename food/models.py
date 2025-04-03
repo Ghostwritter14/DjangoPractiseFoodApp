64257b494052819ca27d80646de0545f35c5c507
soham <sohamrvd@gmail.com>
2025-04-03 19:46:30 +0100

    added images and bootstrap styling

--- food/models.py
+++ food/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Item(models.Model):
     def __str__(self):
         return self.item_name
+
     item_name = models.CharField(max_length=50)
     item_description = models.CharField(max_length=200)
     item_price = models.DecimalField(max_digits=5, decimal_places=2)
+    item_image = models.CharField(max_length=200,
+                                  default="https://www.thefuzzyduck.co.uk/wp-content/uploads/2024/05/image-coming-soon-placeholder-01-660x660.png")
