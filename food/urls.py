276d6e8bc0f198cb2d7effacb41b49ce2484c669
soham <sohamrvd@gmail.com>
2025-04-06 17:36:27 +0100

    hotfixed the templates to render properly and added delete and edit functionality

--- food/urls.py
+++ food/urls.py
@@ -3,7 +3,14 @@ from django.urls import path
 app_name = 'food'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:item_id>', views.detail, name='detail'),
+    # food
+    path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
+    # add items
+    path('add', views.create_item, name='create_item'),
+    #edit
+    path('update/<int:item_id>/', views.edit_item, name='edit_item'),
+    path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
+
 ]
 
--- food/views.py
+++ food/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import ItemForm
 from .models import Item
 from django.template import loader
 
@@ -21,3 +23,26 @@ def detail(request, item_id):
         'item': item
     }
     return render(request, 'food/detail.html', context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    return render(request, 'food/item_form.html', {'form': form})
+
+
+def edit_item(request, item_id):
+    item = Item.objects.get(id=item_id)
+    form = ItemForm(request.POST or None, instance=item)
+    if form.is_valid():
+        form.save()
+        return redirect('food:index', item_id)
+    return render(request, 'food/item_form.html', {'form': form, 'item': item})
+
+def delete_item(request, item_id):
+    item = Item.objects.get(id=item_id)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    return render(request, 'food/item_delete.html', {'item': item})
