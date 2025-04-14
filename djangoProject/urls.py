caa0973bcdf55c22a8763aea2f44ca16dddeb9c4
soham <sohamrvd@gmail.com>
2025-04-14 17:09:45 +0100

    adding login for users.

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 from users import views as users_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("food/", include("food.urls")),
     path("register/", users_views.register, name="Register"),
+    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("login/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
 ]
--- users/forms.py
+++ users/forms.py
@@ -2,8 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
+
     class Meta:
         model = User
         fields = ['username', 'email', '<PASSWORD>', '<PASSWORD2>']
