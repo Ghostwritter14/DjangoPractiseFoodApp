258b1cb722317356c94609f48bc8861432bbdd7d
soham <sohamrvd@gmail.com>
2025-04-16 22:10:04 +0100

    more changes wrestling with styling

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -29,8 +29,10 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path("profile/", users_views.profile, name="profile"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [
                    # ... the rest of your URLconf goes here ...
                ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
