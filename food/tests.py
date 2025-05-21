abfdcc00b6abdb744d7c286feae0bd49410f0a95
soham <sohamrvd@gmail.com>
2025-05-21 20:02:22 +0530

    added some tests

--- food/tests.py
+++ food/tests.py
@@ -1,3 +1,126 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Item, Category
+
+class FoodViewsTestCase(TestCase):
+    def setUp(self):
+        # Create a test user and log in
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.client = Client()
+        self.client.login(username='testuser', password='password')
+
+        # Create a Category
+        self.cat = Category.objects.create(name='TestCat', slug='testcat')
+
+        # Create two Items
+        self.item1 = Item.objects.create(
+            item_name='Item One',
+            item_description='First item',
+            item_price=9.99,
+            item_image='http://example.com/img1.png',
+            category=self.cat,
+            ingredients='ing1,ing2'
+        )
+        self.item2 = Item.objects.create(
+            item_name='Item Two',
+            item_description='Second item',
+            item_price=4.50,
+            item_image='http://example.com/img2.png',
+            category=self.cat,
+            ingredients=''
+        )
+
+    def test_landing_page(self):
+        url = reverse('landing')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'food/landing.html')
+
+    def test_index_view_lists_items(self):
+        url = reverse('food:index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        # both items present in context
+        self.assertIn(self.item1, response.context['item_list'])
+        self.assertIn(self.item2, response.context['item_list'])
+        self.assertTemplateUsed(response, 'food/index.html')
+
+    def test_detail_view_shows_ingredients_list(self):
+        url = reverse('food:detail', args=[self.item1.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        # ingredients_list created by splitting
+        self.assertEqual(response.context['ingredients_list'], ['ing1','ing2'])
+        self.assertTemplateUsed(response, 'food/detail.html')
+
+    def test_category_view_filters(self):
+        url = reverse('food:category', args=[self.cat.slug])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        # both items share the same category
+        self.assertQuerysetEqual(
+            response.context['item_list'],
+            map(repr, [self.item1, self.item2]),
+            ordered=False
+        )
+        self.assertContains(response, self.cat.name)
+
+    def test_create_item_view_get_and_post(self):
+        url = reverse('food:create_item')
+        # GET shows the form
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'food/item_form.html')
+
+        # POST creates a new item
+        new_data = {
+            'item_name': 'New Item',
+            'item_description': 'Desc',
+            'item_price': '5.00',
+            'item_image': 'http://example.com/new.png',
+            'category': self.cat.pk,
+            'ingredients': 'a,b'
+        }
+        response = self.client.post(url, new_data)
+        # should redirect to detail of the new object
+        self.assertEqual(response.status_code, 302)
+        new = Item.objects.get(item_name='New Item')
+        self.assertRedirects(response,
+            reverse('food:detail', args=[new.pk])
+        )
+
+    def test_edit_item_view_get_and_post(self):
+        url = reverse('food:edit_item', args=[self.item1.pk])
+        # GET pre-fills the form
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Item One')
+
+        # POST updates description
+        data = {
+            'item_name': 'Item One',
+            'item_description': 'Updated',
+            'item_price': '9.99',
+            'item_image': self.item1.item_image,
+            'category': self.cat.pk,
+            'ingredients': 'ing1,ing2'
+        }
+        response = self.client.post(url, data)
+        self.assertRedirects(response, reverse('food:detail', args=[self.item1.pk]))
+        self.item1.refresh_from_db()
+        self.assertEqual(self.item1.item_description, 'Updated')
+
+    def test_delete_item_view_get_and_post(self):
+        url = reverse('food:delete_item', args=[self.item2.pk])
+        # GET shows confirmation
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'food/item_delete.html')
+
+        # POST deletes and redirects to index
+        response = self.client.post(url)
+        self.assertRedirects(response, reverse('food:index'))
+        with self.assertRaises(Item.DoesNotExist):
+            Item.objects.get(pk=self.item2.pk)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,86 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from food.models import Category, Item
+from users.models import Profile
+from users.forms import RegisterForm
+from users.views import register, profile as profile_view
+from users.forms import RegisterForm
+from food.models import CustomCombo
 
-# Create your tests here.
+
+class UsersViewsTestCase(TestCase):
+    def setUp(self):
+        # Prepare client and a default category & items
+        self.client = Client()
+        self.cat1 = Category.objects.create(name='Cat1', slug='cat1')
+        self.cat2 = Category.objects.create(name='Cat2', slug='cat2')
+        self.item1 = Item.objects.create(
+            item_name='I1', item_description='D1',
+            item_price=1.0, item_image='url1', category=self.cat1
+        )
+        self.item2 = Item.objects.create(
+            item_name='I2', item_description='D2',
+            item_price=2.0, item_image='url2', category=self.cat2
+        )
+
+    def test_register_get_and_post(self):
+        url = reverse('Register')
+        # GET shows form
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertIsInstance(resp.context['form'], RegisterForm)
+
+        # POST valid data
+        data = {
+            'username': 'newuser',
+            'email': 'new@example.com',
+            'password1': 'complexPassword123',
+            'password2': 'complexPassword123'
+        }
+        resp = self.client.post(url, data)
+        # should redirect to login
+        self.assertRedirects(resp, reverse('login'))
+        # user created
+        u = User.objects.get(username='newuser')
+        # profile auto-created
+        self.assertTrue(hasattr(u, 'profile'))
+        self.assertIsInstance(u.profile, Profile)
+
+    def test_profile_requires_login(self):
+        url = reverse('profile')
+        resp = self.client.get(url)
+        # should redirect to login
+        self.assertRedirects(resp, f"{reverse('login')}?next={url}")
+
+    def test_profile_set_favorite(self):
+        u = User.objects.create_user(username='u2', password='pass')
+        self.client.login(username='u2', password='pass')
+        # choose cat1 as favorite
+        resp = self.client.post(
+            reverse('profile'),
+            {'favorite': self.cat1.id}
+        )
+        self.assertRedirects(resp, reverse('profile'))
+        u.refresh_from_db()
+        self.assertEqual(u.profile.favorite, self.cat1)
+
+    def test_profile_create_combo(self):
+        u = User.objects.create_user(username='u3', password='pass')
+        self.client.login(username='u3', password='pass')
+        # POST combo name and items
+        resp = self.client.post(
+            reverse('profile'),
+            {
+                'name': 'ComboX',
+                'items': [self.item1.id, self.item2.id]
+            }
+        )
+        self.assertRedirects(resp, reverse('profile'))
+        # combo created for user
+        combos = u.custom_combos.all()
+        self.assertEqual(combos.count(), 1)
+        combo = combos.first()
+        self.assertEqual(combo.name, 'ComboX')
+        ids = list(combo.items.values_list('id', flat=True))
+        self.assertCountEqual(ids, [self.item1.id, self.item2.id])
